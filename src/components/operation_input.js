e371723a47eadc5cd6b119c137ce7ac1f89ea85f
raminder goraya <raminder.goraya@gmail.com>
2018-10-01 14:19:21 -0700

    Updates

--- src/components/operation_input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const OperationInput = (props) => {
-  return (
-    <input type="number" className="form-control" data-denominationtype={props.denomination.type} min="0" autoFocus={props.denomination.type === "Hundreds"} />
-  )  
-}
-
-export default OperationInput;
